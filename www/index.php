521539f062ceabf5b5d59e658be84eed9d7b43a5
Stijn-Van-Peer <56153683+Stijn-Van-Peer@users.noreply.github.com>
2019-12-07 15:31:15 +0100

    Update index.php

--- www/index.php
+++ www/index.php
@@ -1,25 +1,19 @@
 <html>
  <head>
-  <title>Hello and welcome</title>
-
+  <title>Linux project</title>
   <meta charset="utf-8">
-  <meta name="author" content="RVolders">
+  <meta name="author" content="Stijn Van Peer">
   <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
   <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-
 </head>
 <body>
     <div class="container">
-    <?php echo "<h1>Welcome to my database :</h1>"; ?>
+    <?php echo "<h1>My cryptocurrency whatchlist;</h1>"; ?>
     <?php
-
-    // Connexion et sélection de la base
     $conn = mysqli_connect('db', 'user', 'R1234-56', "myDb");
-
-    $query = 'SELECT * From Person';
+    $query = 'SELECT * From Cryptocurrency';
     $result = mysqli_query($conn, $query);
-
     echo '<table class="table table-striped">';
     echo '<thead><tr><th></th><th>id</th><th>name</th></tr></thead>';
     while($value = $result->fetch_array(MYSQLI_ASSOC)){
@@ -28,14 +22,11 @@
         foreach($value as $element){
             echo '<td>' . $element . '</td>';
         }
-
         echo '</tr>';
     }
     echo '</table>';
     $result->close();
-
     mysqli_close($conn);
-
     ?>
 	</div>
 </body>
