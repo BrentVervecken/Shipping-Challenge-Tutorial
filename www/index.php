a6d463a521fdbddfb57ab7b4c94f4285fc0df44f
BrentVervecken <45164912+BrentVervecken@users.noreply.github.com>
2019-12-08 10:39:10 +0100

    Update index.php

--- www/index.php
+++ www/index.php
@@ -2,17 +2,17 @@
  <head>
   <title>Linux project</title>
   <meta charset="utf-8">
-  <meta name="author" content="Stijn Van Peer">
+  <meta name="author" content="Brent Vervecken">
   <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
   <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 </head>
 <body>
     <div class="container">
-    <?php echo "<h1>My cryptocurrency whatchlist;</h1>"; ?>
+    <?php echo "<h1>Mijn fietsen database:</h1>"; ?>
     <?php
     $conn = mysqli_connect('db', 'user', 'R1234-56', "myDb");
-    $query = 'SELECT * From Cryptocurrency';
+    $query = 'SELECT * From Fietsen';
     $result = mysqli_query($conn, $query);
     echo '<table class="table table-striped">';
     echo '<thead><tr><th></th><th>id</th><th>name</th></tr></thead>';
